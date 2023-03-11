f25c668a2c16a84c355e4ccadb2e0a23338dc788
Stif <stiven.maldonado@symplifica.com>
2023-03-11 13:30:15 -0500

    call service

--- app/services/products_api_service.rb
+++ app/services/products_api_service.rb
@@ -1,29 +1,55 @@
 class ProductsApiService
 
   require 'rest-client'
-  attr_accessor :products, :response, :filter
+  attr_accessor :products, :response
 
   RETURNS = [
     SUCCESS = :success,
     FAILED = :failed
   ].freeze
 
-  def initialize(filter: nil)
-    self.filter = filter
+  def initialize
+    self.products = []
   end
 
   def call
     begin
-      self.response = RestClient.get('https://dummyjson.com/products')
-      self.products = JSON.parse(response)["products"]
-      apply_filter unless filter.nil?
+      response_dumy = RestClient.get('https://dummyjson.com/products')
+      response_faker = RestClient.get('https://fakestoreapi.com/products')
+
+      products_dumy = JSON.parse(response_dumy)["products"]
+      products_faker = JSON.parse(response_faker)
+      unify_products(products_dumy, products_faker)
     rescue => e
       { status: FAILED, response: { data: e.to_s } }
     end
     { status: SUCCESS, response: { data: products } }
   end
 
-  def apply_filter
-    binding.pry
+  def unify_products(products_dumy, products_faker)
+    products_dumy.each do |detail|
+      self.products.push << {
+        :id => products + 1,
+        :title => detail["title"],
+        :description => detail["description"],
+        :price => detail["price"],
+        :discountPercentage => detail["discountPercentage"],
+        :stock => detail["stock"],
+        :category => detail["category"],
+        :thumbnail => detail["thumbnail"]
+      }
+    end
+    products_faker.each do |detail|
+      self.products.push << {
+        :id => products.count + 1,
+        :title => detail["title"],
+        :description => detail["description"],
+        :price => detail["price"],
+        :discountPercentage => 0,
+        :stock => detail["rating"]["count"],
+        :category => detail["category"],
+        :thumbnail => detail["image"]
+      }
+    end
   end
 end
